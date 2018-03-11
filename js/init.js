48671c860e9adc9aa075a186c46700509e9e26a2
Amit Nambiar <amit.lzkpa@gmail.com>
2018-03-10 20:57:17 -0500

    first commit

---
+++ js/init.js
@@ -0,0 +1,21 @@
+(function($){
+  $(function(){
+
+
+  	var sliderOpts =  {
+  		indicators: false,
+  		height: 600,
+  		duration: 1000,
+  		interval: 4000
+  	};
+    $('.slider').slider(sliderOpts);
+
+
+  	var $banner = $("#banner");
+  	// $banner.css('background-color', 'blue');
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
